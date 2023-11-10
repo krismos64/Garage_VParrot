3185ba93695bfe0d09a82e949b6e2853d9996945
krismos64 <134715062+krismos64@users.noreply.github.com>
2023-11-10 11:36:54 +0100

    slugTrait

--- src/Entity/Car.php
+++ src/Entity/Car.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Entity\Trait\SlugTrait;
 use App\Repository\CarRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -11,6 +12,8 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: CarRepository::class)]
 class Car
 {
+    use SlugTrait;
+    
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
