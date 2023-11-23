675840a1c39ab292605c72052f6e660ef2cbb5ca
krismos64 <134715062+krismos64@users.noreply.github.com>
2023-11-23 06:28:09 +0100

    amelioration Front

---
+++ src/Controller/FAQController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Schedules;
+use App\Repository\SchedulesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FAQController extends AbstractController
+{
+    #[Route('/foire-aux-questions', name: 'app_f_a_q')]
+    public function index(SchedulesRepository $schedulesRepository,): Response
+    {
+    
+        $days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+        $workingHours = [];
+        foreach ($days as $day) {
+            $workingHours[$day] = $schedulesRepository->findWorkingHoursByDay($day);
+        }
+
+        return $this->render('FAQ/index.html.twig', [
+            'workingHours' => $workingHours,
+            
+        ]);
+    }
+}
